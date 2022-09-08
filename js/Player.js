68e90e31079f91eba72ba9d5577af79ab9a558ac
Won Lee <whd793@gmail.com>
2022-09-08 13:42:16 +0900

    added game logic

--- js/Player.js
+++ js/Player.js
@@ -7,6 +7,7 @@ class Player {
         this.tokens = this.createTokens(21);
     }
     
+    
    /**
      * Creates token objects for player
      * @param   {integer}   num - Number of token objects to be created
@@ -21,5 +22,32 @@ class Player {
         }
         
         return tokens;
+    }
+    
+    
+    /**
+     * Gets all tokens that haven't been dropped.
+     * @return {array}  Array of unused tokens.
+     */
+    get unusedTokens(){
+        return this.tokens.filter(token => !token.dropped);
+    }
+    
+    
+    /**
+     * Gets the active token by returning the first token in the array of unused tokens.
+     * @return {Object} First token object in the array of unused tokens.
+     */
+	get activeToken() {
+        return this.unusedTokens[0];
+    }
+    
+
+    /**
+     * Check if a player has any undropped tokens left
+     * @return {Boolean} 
+     */
+    checkTokens(){
+        return this.unusedTokens.length == 0 ? false : true;
     }
 }
